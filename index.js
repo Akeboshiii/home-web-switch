77844087baa22c23f2f48b9c7162eba125bdc2e4
Akeboshiii <johnkuraya491@gmail.com>
2025-09-27 02:32:29 +0800

    Implement server-side data handling and add client-side event listeners for state updates

--- index.js
+++ index.js
@@ -9,7 +9,24 @@ const PORT = 3000
 const __filename = url.fileURLToPath(import.meta.url)
 export const __dirname = path.dirname(__filename)
 
+const dataFile = path.resolve(__dirname, 'public/data.json')
+
+app.use(express.json())
+
 app.use('/', express.static(path.resolve(__dirname, 'public')))
+app.post('/', async (req, res) => {
+    try {
+        let data = []
+    
+        const file = await fs.readFile(dataFile, 'utf-8')
+        data = JSON.parse(file)
+        data.push(req.body)
+    
+        await fs.writeFile(dataFile, JSON.stringify(data, null, 2))
+    } catch (error) {
+        res.status(500).send('Failed to save')
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`port: ${PORT}`)
---
+++ public/js/index.js
@@ -0,0 +1,45 @@
+const r1_on = document.querySelector('#r1_on')
+const r2_on = document.querySelector('#r1_off')
+const r3_on = document.querySelector('#r2_on')
+const r4_on = document.querySelector('#r2_off')
+
+let stat = {
+    r1: false,
+    r2: false
+}
+
+r1_on.addEventListener('click', () => {
+    stat.r1 = true
+    fetch('/', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(stat)
+    })
+})  
+
+r1_off.addEventListener('click', () => {
+    stat.r1 = false
+    fetch('/', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(stat)
+    })
+})  
+
+r2_on.addEventListener('click', () => {
+    stat.r2 = true
+    fetch('/', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(stat)
+    })
+})  
+
+r1_off.addEventListener('click', () => {
+    stat.r2 = false
+    fetch('/', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(stat)
+    })
+})  
