0c9885c8c987852726defa915f9260de8b94073d
Akeboshiii <johnkuraya491@gmail.com>
2025-09-29 02:53:23 +0800

    feat: enhance UI layout and add authentication functionality in index.html and index.js

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,23 @@ const r1_off = document.querySelector('#r1_off')
 const r2_on = document.querySelector('#r2_on')
 const r2_off = document.querySelector('#r2_off')
 
+const username = document.querySelector('#username')
+const password = document.querySelector('#password')
+const login = document.querySelector('#login')
+const auth = document.querySelector('.auth_container')
+const note = document.querySelector('#notes')
+
+login.addEventListener('click', () => {
+    // sa front end lol HAHAHAHA
+    if (username.value === 'yrua_locsin' && password.value === 'P@ssword') {
+        auth.style.display = 'none'
+    } else {
+        note.textContent = 'Wrong credentials please try again'
+        username.value = ''
+        password.value = ''
+    }
+})
+
 let stat = {
     r1: false,
     r2: false
@@ -10,6 +27,7 @@ let stat = {
 
 r1_on.addEventListener('click', () => {
     stat.r1 = true
+    r1.setAttribute('src', 'images/light_on.png')
     fetch('/', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
@@ -19,6 +37,7 @@ r1_on.addEventListener('click', () => {
 
 r1_off.addEventListener('click', () => {
     stat.r1 = false
+    r1.setAttribute('src', 'images/light_off.png')
     fetch('/', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
@@ -28,6 +47,7 @@ r1_off.addEventListener('click', () => {
 
 r2_on.addEventListener('click', () => {
     stat.r2 = true
+    r2.setAttribute('src', 'images/outlet_on.png')
     fetch('/', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
@@ -37,9 +57,13 @@ r2_on.addEventListener('click', () => {
 
 r2_off.addEventListener('click', () => {
     stat.r2 = false
+    r2.setAttribute('src', 'images/outlet_off.png')
     fetch('/', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(stat)
     })
 })  
+
+const r1 = document.querySelector("#r1")
+const r2 = document.querySelector("#r2")
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json())
 // app.get('/', (req, res) => {
 //   res.send('good')
 // })
-
+        
 console.log(__dirname)
 
 app.post('/', (req, res) => {
